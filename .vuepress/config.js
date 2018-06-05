7f7a2defb54e43eced574b08210bc3818f30e7d9
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-06-05 15:05:25 -0500

    update build command

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,20 +2,21 @@ module.exports = {
   title: 'Jangle',
   description: 'a cms for humans.',
   head: [
-    [ 'link', {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro:300,600|Source+Sans+Pro:300,300i,600,600i'
-    }],
-    [ 'style', { type: 'text/css' }, `
-      body {  font-family: "Source Sans Pro", sans-serif !important; }
-    `]
+    [
+      'link', {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro:300,600|Source+Sans+Pro:300,300i,600,600i'
+      }
+    ]
   ],
   themeConfig: {
     nav: [
       { text: 'Guide', link: '/guide/' },
-      { text: 'Docs', items: [
-        { text: 'Jangle API', link: '/docs/api/' }
-       ] }
+      {
+        text: 'Docs', items: [
+          { text: 'Jangle API', link: '/docs/api/' }
+        ]
+      }
     ],
     docsRepo: 'jangle-cms/io',
     // if your docs are in a specific branch (defaults to 'master'):
